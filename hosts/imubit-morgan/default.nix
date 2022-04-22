eeb1c679ba3faec7b8aaf7a9c7e0f8a5a0fb6831
System administrator <root@imubit-morgan.localdomain>
2022-04-22 22:23:27 +0000

    add wsl files

---
+++ hosts/imubit-morgan/default.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  nixos-wsl = import ./nixos-wsl;
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+
+    nixos-wsl.nixosModules.wsl
+  ];
+
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "mhelton";
+    startMenuLaunchers = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker.enable = true;
+  };
+
+  # Enable nix flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  environment.systemPackages = with pkgs; [ git man ];
+}
