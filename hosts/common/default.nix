37ce11e538be3887f5b2a7d5d1ea39cd7534df30
Morgan Helton <mhelton@gmail.com>
2022-04-22 22:47:35 -0500

    install age

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -13,7 +13,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    age
+    age 
   ];
 
   # enable passwordless sudo
