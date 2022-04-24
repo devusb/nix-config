7777c9d9a2833b807c06cfdd31339f56c4cf803b
Morgan Helton <mhelton@gmail.com>
2022-04-24 14:51:41 -0500

    final workaround for hidpi + utm (for now)

--- hosts/common/aarch64-vm.nix
+++ hosts/common/aarch64-vm.nix
@@ -1,71 +1,53 @@
-{ pkgs, inputs, ... }: {
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
+{ pkgs, inputs, lib, ... }: 
+  let
+    mkSure = lib.mkOverride 0;
+  in {
+    services.qemuGuest.enable = true;
+    services.spice-vdagentd.enable = mkSure true;
 
-  # add GUI
-  environment.pathsToLink = [ "/libexec" ];
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    libinput = {  
+    # add GUI
+    environment.pathsToLink = [ "/libexec" ];
+    services.xserver = {
       enable = true;
-      touchpad.naturalScrolling = true;
-      mouse.naturalScrolling = true;
+      layout = "us";
+      dpi = 180;
+      libinput = {  
+        enable = true;
+        touchpad.naturalScrolling = true;
+        mouse.naturalScrolling = true;
+      };
+
+      desktopManager = {
+        xterm.enable = false;
+      };
+
+      displayManager = {
+        defaultSession = "none+i3";
+        autoLogin = {
+          user = "mhelton";
+          enable = true;
+        };
+        sessionCommands = "spice-vdagent";
+      };
+      
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu 
+          i3status 
+          i3lock 
+      ];
+        extraSessionCommands = "${pkgs.xorg.xrandr}/bin/xrandr --dpi 180";
+      };
     };
 
-    desktopManager = {
-      xterm.enable = false;
+  # hidpi
+    console.font =
+      "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+    environment.variables = {
+      GDK_SCALE = "2";
+      GDK_DPI_SCALE = "0.5";
+      _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
     };
-
-    displayManager = {
-      defaultSession = "none+i3";
-       autoLogin = {
-         user = "mhelton";
-         enable = true;
-       };
-       sessionCommands = "spice-vdagent";
-    };
-    
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu 
-        i3status 
-        i3lock 
-     ];
-      #extraSessionCommands = "spice-vdagent";
-      extraSessionCommands = "${pkgs.xorg.xrandr}/bin/xrandr --dpi 180";
-    };
-  };
-
- # hidpi
-  console.font =
-    "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
-  };
-  # hardware.video.hidpi.enable = true;
-  # services.xserver.dpi = 180;
-
-  # services.autorandr = {
-  #   enable = true;
-  #   defaultTarget = "utm";
-  #   profiles = {
-  #     "utm" = {
-  #       fingerprint = {
-  #         Virtual-1 = "";
-  #       };
-  #       config = {
-  #         Virtual-1 = {
-  #           enable = true;
-  #           dpi = 180;
-  #           mode = "640x480";
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
 
 }
