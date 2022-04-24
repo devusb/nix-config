868692e97240ac9fb11c4011210c7231ee70eb44
Morgan Helton <mhelton@gmail.com>
2022-04-24 17:05:46 -0500

    enable kitty as an option

--- users/mhelton/home/graphical.nix
+++ users/mhelton/home/graphical.nix
@@ -1,5 +1,8 @@
 { pkgs, ...}: {
-    home.packages = with pkgs; [lens kitty];
+    home.packages = with pkgs; [lens];
     programs.google-chrome.enable = true;
     programs.vscode.enable = true;
+    programs.kitty = {
+    	enable = true;
+    };
 }
