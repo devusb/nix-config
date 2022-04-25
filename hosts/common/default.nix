8c1ab40bd71f887137729d8728c846c5eca4877d
Morgan Helton <morgan@imubit.com>
2022-04-25 14:16:14 -0500

    install nfs

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -12,6 +12,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    nfs-utils
   ];
 
   # enable passwordless sudo
