e4a922b4dcef75d9e7538e80e3c5c4d9f7d9a1d4
devusb <mhelton@gmail.com>
2022-04-27 08:21:08 -0500

    move WSL bits to common (#8)
    Co-authored-by: Morgan Helton <morgan@imubit.com>

--- hosts/imubit-morgan/default.nix
+++ hosts/imubit-morgan/default.nix
@@ -1,23 +1,6 @@
-{ lib, pkgs, config, modulesPath, ... }:
-
-with lib;
-let
-  nixos-wsl = import ./nixos-wsl;
-in
-{
+{ pkgs, inputs, ... }: {
   imports = [
-    "${modulesPath}/profiles/minimal.nix"
     ../common
-    nixos-wsl.nixosModules.wsl
+    ../common/wsl.nix
   ];
-
-  wsl = {
-    enable = true;
-    automountPath = "/mnt";
-    defaultUser = "mhelton";
-    startMenuLaunchers = true;
-    docker-native.enable = true;
-    # Enable integration with Docker Desktop (needs to be installed)
-    # docker.enable = true;
-  };
 }
