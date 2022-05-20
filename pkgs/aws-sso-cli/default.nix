2a0a661b31ee4abf543e03f51e74624a6d018322
Morgan Helton <mhelton@gmail.com>
2022-05-20 17:40:28 -0500

    working aws-sso-cli

--- pkgs/aws-sso-cli/default.nix
+++ pkgs/aws-sso-cli/default.nix
@@ -1,20 +1,23 @@
+{ buildGoModule, fetchFromGitHub, lib }:
+  buildGoModule rec {
+    pname = "aws-sso-cli";
+    version = "1.9.2";
 
-  { buildGoModule, fetchFromGitHub, lib }:
+    src = fetchFromGitHub {
+      owner = "synfinatic";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "9/dZfRmFAyE5NEMmuiVsRvwgqQrTNhXkTR9N0d3zgfk=";
+    };
+    vendorSha256 = "BlSCLvlrKiubMtfFSZ5ppMmL2ZhJcBXxJfeRgMADYB4=";
 
-buildGoModule rec {
-  pname = "aws-sso-cli";
-  version = "1.9.2";
+    postInstall = ''
+      mv $out/bin/cmd $out/bin/aws-sso
+    '';
 
-  src = fetchFromGitHub {
-    owner = "synfinatic";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "9/dZfRmFAyE5NEMmuiVsRvwgqQrTNhXkTR9N0d3zgfk=";
-  };
-
-  meta = with lib; {
-    homepage = "https://github.com/synfinatic/aws-sso-cli";
-    description = "AWS SSO CLI is a secure replacement for using the aws configure sso wizard.";
-    license = licenses.gpl3;
-  };
+    meta = with lib; {
+      homepage = "https://github.com/synfinatic/aws-sso-cli";
+      description = "AWS SSO CLI is a secure replacement for using the aws configure sso wizard.";
+      license = licenses.gpl3;
+    };
 }
