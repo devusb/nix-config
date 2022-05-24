b0872af0cea70218e98b4a6f7e50e0bf9e85a1cc
devusb <mhelton@gmail.com>
2022-05-23 20:57:01 -0500

    delete superintendent-vm

--- hosts/superintendent-vm/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, ... }: {
-  imports = [
-    ./hardware-configuration.nix
-    ../common
-    ../common/aarch64-vm.nix
-    ../common/docker.nix
-  ];
-  system.stateVersion = "21.11";
-}
