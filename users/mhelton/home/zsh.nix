ba9c1cfec37dd9fccb25f44cfc5976374b8b585f
Morgan Helton <mhelton@gmail.com>
2022-05-24 13:34:55 -0500

    enable exa icons

--- users/mhelton/home/zsh.nix
+++ users/mhelton/home/zsh.nix
@@ -5,8 +5,8 @@
         enableSyntaxHighlighting = true;
         enableCompletion = true;
         shellAliases = {
-            ll = "exa -l --git";
-            ls = "exa";
+            ll = "exa -l --git --icons";
+            ls = "exa --icons";
             update = if system == "aarch64-darwin" then "darwin-rebuild switch --flake ~/code/nix-config/ && rm result" else "nixos-rebuild switch --use-remote-sudo --flake /dotfiles/";
             update-home = if system == "aarch64-darwin" then "home-manager switch --flake ~/code/nix-config/" else "home-manager switch --flake /dotfiles/";
             kb = "kubectl";
