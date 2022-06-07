978ae396150faaf7ea353027f3d82815fa033433
Morgan Helton <morgan@imubit.com>
2022-06-07 11:33:27 -0500

    nix-shell zsh

--- users/mhelton/home/zsh.nix
+++ users/mhelton/home/zsh.nix
@@ -26,6 +26,18 @@
             fi
         ''
         ;
+        plugins = [
+	      {
+	        name = "zsh-nix-shell";
+	        file = "nix-shell.plugin.zsh";
+	        src = pkgs.fetchFromGitHub {
+	          owner = "chisui";
+	          repo = "zsh-nix-shell";
+	          rev = "v0.5.0";
+	          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+	        };
+	      }	
+        ];
     };
     programs.keychain = {
         enable = true;
