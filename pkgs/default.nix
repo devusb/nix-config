92849713f17a096bf72c8f0e6c1cfde6716f4eb0
Morgan Helton <morgan@imubit.com>
2022-06-08 07:18:07 -0500

    add helm 2.16.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
 { pkgs }: {
   helm2_15_1 = pkgs.callPackage ./helm2 { version = "2.15.1"; };
   helm2_13_1 = pkgs.callPackage ./helm2 { version = "2.13.1"; };
+  helm2_16_2 = pkgs.callPackage ./helm2 { version = "2.16.2"; };
   aws-sso-cli = pkgs.callPackage ./aws-sso-cli {};
 }
