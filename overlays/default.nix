ead901946536f2ae834b1ea135b5633f70e2c5f1
Morgan Helton <mhelton@gmail.com>
2022-06-11 13:09:27 -0500

    override jsonschema to not include twisted

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,9 @@
 { inputs, ... }: final: prev: {
   stable = import inputs.nixpkgs-stable { system = prev.system; };
+  
+  python39 = prev.python39.override (rec {
+    packageOverrides = final: prev: {
+      jsonschema = prev.jsonschema.overrideAttrs (old: {checkInputs = [];});
+    };
+  });
 } // import ../pkgs { pkgs = final; }
