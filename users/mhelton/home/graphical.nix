060c1f78cc684df568013ddfd67d8a82feb0b14b
Morgan Helton <mhelton@gmail.com>
2022-06-15 14:43:54 -0500

    install zoom

--- users/mhelton/home/graphical.nix
+++ users/mhelton/home/graphical.nix
@@ -3,7 +3,7 @@
       ./terminal.nix
   ];
 
-  home.packages = with pkgs; [lens gnome.adwaita-icon-theme gnome.gnome-tweaks];
+  home.packages = with pkgs; [lens gnome.adwaita-icon-theme gnome.gnome-tweaks zoom-us];
   programs.google-chrome.enable = true;
   programs.vscode.enable = true;
 
