4d90e6320e49843b1bb4a420835b346859fb4171
Morgan Helton <mhelton@gmail.com>
2022-06-25 18:08:03 -0500

    add placeholder for darwin-specific packages

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -1,3 +1,6 @@
 { pkgs, config, system, ...}: {
-    xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = ./extra/iterm2.conf;
+  xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = ./extra/iterm2.conf;
+  home.packages = with pkgs; [ 
+    
+  ];
 }
