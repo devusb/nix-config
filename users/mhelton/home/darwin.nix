50cf7445ebccf1c2980079d32d06b71127eb9749
Morgan Helton <mhelton@gmail.com>
2022-06-26 12:25:42 -0500

    restructure iterm2 settings, run nixpkgs-fmt again

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -1,6 +1,6 @@
 { pkgs, config, system, lib, ... }: {
   xdg.configFile = {
-    "iterm2/com.googlecode.iterm2.plist".text = lib.generators.toPlist {} (import ./iterm2.nix);
+    "iterm2/com.googlecode.iterm2.plist".text = lib.generators.toPlist { } (import ./extra/iterm2.nix);
   };
   home.packages = with pkgs; [
   ];
