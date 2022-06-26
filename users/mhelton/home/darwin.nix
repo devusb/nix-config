cfe06a895f983e1283f7479477c532bb2be07d49
Morgan Helton <mhelton@gmail.com>
2022-06-26 14:00:15 -0500

    split iterm2 profile

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -1,6 +1,7 @@
 { pkgs, config, system, lib, ... }: {
   xdg.configFile = {
     "iterm2/com.googlecode.iterm2.plist".text = lib.generators.toPlist { } (import ./extra/iterm2.nix);
+    "iterm2/AppSupport/DynamicProfiles/mhelton.plist".text = lib.generators.toPlist { } (import ./extra/iterm2_profile.nix);
   };
   home.packages = with pkgs; [
   ];
