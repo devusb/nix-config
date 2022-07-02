75f549543a77384074f8d02892db08e869b7b50e
Morgan Helton <mhelton@gmail.com>
2022-07-02 12:58:33 -0500

    add detectindent micro plugin

--- users/mhelton/home/micro.nix
+++ users/mhelton/home/micro.nix
@@ -23,6 +23,14 @@
           rev = "v1.0.0";
           sha256 = "sha256-ZNq2QH4TnmJkkV6aSoNNGUAB/M7wJuTNVU1539mLAOk=";
         };
+    "micro/plug/detectindent".source =
+      pkgs.fetchFromGitHub
+        {
+          owner = "dmaluka";
+          repo = "micro-detectindent";
+          rev = "v1.1.0";
+          sha256 = "sha256-5bKEkOnhz0pyBR2UNw5vvYiTtpd96fBPTYW9jnETvq4=";
+        };
   };
 
 
