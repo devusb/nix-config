ad206c42e732846f2dfbd19ce24564b5be832c72
Morgan Helton <mhelton@gmail.com>
2022-07-08 18:08:42 -0500

    remove cmdg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,4 @@
   helm2_13_1 = pkgs.callPackage ./common/helm2 { version = "2.13.1"; };
   helm2_16_2 = pkgs.callPackage ./common/helm2 { version = "2.16.2"; };
   brave = if prev.system == "aarch64-darwin" then prev.callPackage ./darwin/brave { } else prev.brave;
-  cmdg = pkgs.callPackage ./common/cmdg { };
 }
