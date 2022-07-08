0198d75469e985559fdbe58cbbda0b14e9fbc79c
Morgan Helton <mhelton@gmail.com>
2022-07-07 21:04:23 -0500

    make arch-specific home-manager configs exclusive

--- users/mhelton/home/default.nix
+++ users/mhelton/home/default.nix
@@ -8,9 +8,10 @@
   ]
   ++ (if gaming == true then [ ./gaming.nix ] else [ ])
   ++ (if graphical == true then [ ./graphical.nix ] else [ ])
-  ++ (if system == "aarch64-linux" then [ ./aarch64-vm.nix ] else [ ])
   ++ (if work == true then [ ./work.nix ] else [ ./personal.nix ])
-  ++ (if system == "x86_64-linux" then [ ./linux.nix ] else [ ./darwin.nix ]);
+  ++ (if system == "aarch64-linux" then [ ./aarch64-vm.nix ] else [ ])
+  ++ (if system == "x86_64-linux" then [ ./linux.nix ] else [ ])
+  ++ (if system == "aarch64-darwin" then [ ./darwin.nix ] else [ ]);
 
   fonts.fontconfig.enable = true;
 
