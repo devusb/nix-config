652d220558846ab1b1ecf0851fa2608904a89e30
Morgan Helton <mhelton@gmail.com>
2022-07-10 13:10:48 -0500

    enable ip forwarding on linux

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -19,6 +19,9 @@
   services.tailscale = {
     enable = true;
   };
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
 
   # enable passwordless sudo
   security.sudo.wheelNeedsPassword = false;
