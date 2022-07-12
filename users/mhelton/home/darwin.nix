8a8df00c30aacef69f76f67d9fe182876305b6fc
Morgan Helton <morgan@imubit.com>
2022-07-12 14:16:45 -0500

    add iterm2 shell integration

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -9,11 +9,23 @@
     preferences = import ./extra/iterm2.nix;
   };
 
-  programs.zsh ={
+  programs.zsh = {
     shellAliases = {
       tsup = "sudo tailscale up && networksetup -setdnsservers Wi-Fi 100.100.100.100";
       tsdn = "sudo tailscale down && networksetup -setdnsservers Wi-Fi Empty";
     };
+    plugins = [
+      {
+        name = "iterm2";
+        file = "Resources/shell_integration/iterm2_shell_integration.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "gnachman";
+          repo = "iTerm2";
+          rev = "698b83ec79d882ee5acb25b7e358680f9db47e01";
+          sha256 = "sha256-86h4zVRjjQmPXuLsbTycdJeGhlhRumSDK33t4lMYZ78=";
+        };
+      }
+    ];
   };
 
   home.sessionVariables = {
