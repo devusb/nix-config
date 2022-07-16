47560a6a1c96f4859974c902250c7614e5117099
Morgan Helton <mhelton@gmail.com>
2022-07-15 23:22:02 -0500

    add accept routes and qr codes to tsup

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -11,7 +11,7 @@
 
   programs.zsh ={
     shellAliases = {
-      tsup = "sudo tailscale up && networksetup -setdnsservers Wi-Fi 100.100.100.100";
+      tsup = "sudo tailscale up --accept-routes && networksetup -setdnsservers Wi-Fi 100.100.100.100";
       tsdn = "sudo tailscale down && networksetup -setdnsservers Wi-Fi Empty";
     };
   };
