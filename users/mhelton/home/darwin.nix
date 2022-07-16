282fc47af50117b6d08607223a12f16c82245643
Morgan Helton <mhelton@gmail.com>
2022-07-15 23:23:00 -0500

    actually add -qr for login

--- users/mhelton/home/darwin.nix
+++ users/mhelton/home/darwin.nix
@@ -11,7 +11,7 @@
 
   programs.zsh ={
     shellAliases = {
-      tsup = "sudo tailscale up --accept-routes && networksetup -setdnsservers Wi-Fi 100.100.100.100";
+      tsup = "sudo tailscale up --accept-routes --qr && networksetup -setdnsservers Wi-Fi 100.100.100.100";
       tsdn = "sudo tailscale down && networksetup -setdnsservers Wi-Fi Empty";
     };
   };
