3a5d7ece80f6638ac71bde4ce03ea80bd3d69b15
Morgan Helton <morgan@imubit.com>
2022-07-20 17:47:14 -0500

    add helm 2.17

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,6 @@
   helm2_15_1 = pkgs.callPackage ./common/helm2 { version = "2.15.1"; };
   helm2_13_1 = pkgs.callPackage ./common/helm2 { version = "2.13.1"; };
   helm2_16_2 = pkgs.callPackage ./common/helm2 { version = "2.16.2"; };
+  helm2_17_0 = pkgs.callPackage ./common/helm2 { version = "2.17.0"; };  
   brave = if prev.stdenv.isDarwin then prev.callPackage ./darwin/brave { } else prev.brave;
 }
