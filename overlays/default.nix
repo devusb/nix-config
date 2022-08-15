df43e80bea36e5e36970b215280e0c972c9b4aab
Morgan Helton <mhelton@gmail.com>
2022-08-15 17:15:40 -0500

    revert micro to custom repo for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,4 +16,13 @@
           };
         } else prev.python310;
 
+  micro = stable.micro.overrideAttrs (old: {
+    src = prev.fetchFromGitHub {
+      owner = "devusb";
+      repo = "micro";
+      rev = "allow-plugin-symlinks";
+      sha256 = "sha256-WjdFBNL4Vqtrec5snRw3O5sE8HcrM6P4Rn3Cg3+FrdQ=";
+    };
+  });
+
 } // import ../pkgs { pkgs = final; prev = prev; }
