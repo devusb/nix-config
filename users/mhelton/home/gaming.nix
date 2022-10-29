e6195cacf13e6a301b47b8d4a179c8092cb1b31e
Morgan Helton <mhelton@gmail.com>
2022-10-29 15:32:44 -0500

    enable sunshine as service in gaming profile

--- users/mhelton/home/gaming.nix
+++ users/mhelton/home/gaming.nix
@@ -12,4 +12,13 @@
       favorite-apps = [ "steam.desktop" "net.lutris.Lutris.desktop" ];
     };
   };
+
+  systemd.user.services = {
+    sunshine = {
+      Unit.Description = "Sunshine is a Game stream host for Moonlight.";
+      Service.ExecStart = "${pkgs.sunshine}/bin/sunshine";
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
 }
