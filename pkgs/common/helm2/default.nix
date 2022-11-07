d644cf770f5967bb250b21a93128604d45378678
Morgan Helton <mhelton@gmail.com>
2022-11-06 20:16:00 -0600

    complete helm2 for linux

--- pkgs/common/helm2/default.nix
+++ pkgs/common/helm2/default.nix
@@ -1,42 +1,31 @@
-{ lib, stdenv, buildGoPackage, fetchFromGitHub, version ? "2.15.1" }:
-
-buildGoPackage rec {
+{ lib, stdenvNoCC, version, hash, autoPatchelfHook }:
+let
+  arches = {
+    aarch64-darwin = "darwin-amd64";
+    x86_64-darwin = "darwin-amd64";
+    x86_64-linux = "linux-amd64";
+  };
+in
+stdenvNoCC.mkDerivation rec {
   inherit version;
-  pname = "helm";
+  pname = "kubernetes-helm";
 
-  src = fetchFromGitHub {
-    owner = "helm";
-    repo = "helm";
-    rev = "v${version}";
-    sha256 = "1afbymgpax7kgjjv1c9xb4dm7gcrhn2g69piamdq1k0ng348k5w0";
+  src = builtins.fetchTarball {
+    url = "https://get.helm.sh/helm-v${version}-${arches.${stdenvNoCC.hostPlatform.system}}.tar.gz";
+    sha256 = hash.${stdenvNoCC.hostPlatform.system};
   };
 
-  goPackagePath = "k8s.io/helm";
-  subPackages = [ "cmd/helm" "cmd/tiller" "cmd/rudder" ];
+  nativeBuildInputs = [ ] ++ lib.optionals stdenvNoCC.isLinux [ autoPatchelfHook ];
 
-  goDeps = ./deps.nix;
-
-  # Thsese are the original flags from the helm makefile
-  ldflags = [ "-X k8s.io/helm/pkg/version.Version=v${version}" "-X k8s.io/helm/pkg/version.GitTreeState=clean" "-X k8s.io/helm/pkg/version.BuildMetadata=" "-w" "-s" ];
-
-  preBuild = ''
-    # This is a hack(?) to flatten the dependency tree the same way glide or dep would
-    # Otherwise you'll get errors like
-    # have DeepCopyObject() "k8s.io/kubernetes/vendor/k8s.io/apimachinery/pkg/runtime".Object
-    # want DeepCopyObject() "k8s.io/apimachinery/pkg/runtime".Object
-    rm -rf $NIX_BUILD_TOP/go/src/k8s.io/kubernetes/vendor
-    rm -rf $NIX_BUILD_TOP/go/src/k8s.io/apiextensions-apiserver/vendor
-  '';
-
-  postInstall = ''
-    mv $out/bin/helm $out/bin/helm${version}
-    mv $out/bin/rudder $out/bin/rudder${version}
-    mv $out/bin/tiller $out/bin/tiller${version}
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp helm $out/bin/helm${version}
+    runHook postInstall
   '';
 
   meta = with lib; {
-    homepage = https://github.com/kubernetes/helm;
-    description = "A package manager for kubernetes";
-    license = licenses.asl20;
+    platforms = [ "aarch64-darwin" "x86_64-linux" "x86_64-darwin" ];
   };
 }
+
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,28 +1,32 @@
 # When you add custom packages, list them here
 { pkgs, ... }: {
   # helm2 binaries
-  helm2_15_1 = pkgs.callPackage ./darwin/helm2 {
+  helm2_15_1 = pkgs.callPackage ./common/helm2 {
     version = "2.15.1";
     hash = {
       aarch64-darwin = "sha256:0f9ic5cav1nbsgvis891nylqymsj8lcr7g1bvxn9601c3wfk7n2f";
+      x86_64-linux = "sha256:00h6kzig4nmvh7k1wvrwi9cm3ww488jwn0hvqpiqagxp07mfdiw4";
     };
   };
-  helm2_13_1 = pkgs.callPackage ./darwin/helm2 {
+  helm2_13_1 = pkgs.callPackage ./common/helm2 {
     version = "2.13.1";
     hash = {
       aarch64-darwin = "sha256:0a21xigcblhc9wikl7ilqvs7514ds4x71jz4yv2kvv1zjvdd9i8n";
+      x86_64-linux = "sha256:1wyhyxsm7260wjx9lqzg7vhply52m9yb5mcixifx0q4lq3s2pgp4";
     };
   };
-  helm2_16_2 = pkgs.callPackage ./darwin/helm2 {
+  helm2_16_2 = pkgs.callPackage ./common/helm2 {
     version = "2.16.2";
     hash = {
       aarch64-darwin = "sha256:0hqmv2dk15airyihkqzg4x13hndvlff1k7sa1pbr6hgbm8lb6gqh";
+      x86_64-linux = "sha256:0xmkdfrnbxziwcq4s1xwjrk15nihcb7adsz05429k99ws3yn1inr";
     };
   };
-  helm2_17_0 = pkgs.callPackage ./darwin/helm2 {
+  helm2_17_0 = pkgs.callPackage ./common/helm2 {
     version = "2.17.0";
     hash = {
       aarch64-darwin = "sha256:1kz9a35w8gy97v1qgyq6jr7gfhkqn3hc60xknchabhzxjwm6ypr4";
+      x86_64-linux = "sha256:13i29mrhw7vhy1mq08khrcqdzyspic5cabz7h7d2nc2pgxjapf4z";
     };
   };
 
