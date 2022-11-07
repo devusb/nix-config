e3b4c22533918d150032ec280de2b9afe980f29a
Morgan Helton <mhelton@gmail.com>
2022-11-06 20:37:49 -0600

    make tailscale aliases absolute package path

--- home/mhelton/darwin.nix
+++ home/mhelton/darwin.nix
@@ -14,8 +14,8 @@
 
   programs.zsh = {
     shellAliases = {
-      tsup = "sudo tailscale up --accept-routes --qr && networksetup -setdnsservers Wi-Fi 100.100.100.100";
-      tsdn = "sudo tailscale down && networksetup -setdnsservers Wi-Fi Empty";
+      tsup = "sudo ${pkgs.tailscale}/bin/tailscale up --accept-routes --qr && networksetup -setdnsservers Wi-Fi 100.100.100.100";
+      tsdn = "sudo ${pkgs.tailscale}/bin/tailscale down && networksetup -setdnsservers Wi-Fi Empty";
     };
   };
 
