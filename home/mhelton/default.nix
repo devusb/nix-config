dcca4acaade49210a1b6fff96b16e6939162490c
Morgan Helton <morgan@imubit.com>
2022-12-18 09:52:46 -0600

    remove garnix from substituters

--- home/mhelton/default.nix
+++ home/mhelton/default.nix
@@ -19,9 +19,4 @@
     fontconfig.enable = true;
   };
 
-  nix.settings = {
-    substituters = [ "https://cache.nixos.org/" "https://cache.garnix.io/" ];
-    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-  };
-
 }
