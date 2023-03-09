13e4bfa0299c4ba66b1302bc9ff380ce85a3d6b2
Morgan Helton <mhelton@gmail.com>
2023-03-09 05:20:11 -0600

    remove uxplay module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,5 @@
 # Add your home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 {
   iterm2 = ./iterm2.nix;
-  uxplay = ./uxplay.nix;
   shairport-sync = ./shairport-sync.nix;
 }
