95257745adc38d3005b467d4c30895fcce4824fd
Morgan Helton <mhelton@gmail.com>
2023-03-17 10:21:30 -0500

    neovim plugin and keybinds changes

--- home/mhelton/neovim.nix
+++ home/mhelton/neovim.nix
@@ -13,20 +13,23 @@
     };
     plugins = {
       treesitter.enable = true;
-      telescope.enable = true;
+      telescope = {
+        enable = true;
+        extensions.fzf-native.enable = true;
+        extensions.fzf-native.fuzzy = true;
+      };
       nvim-autopairs = {
         enable = true;
         checkTs = true;
       };
-      neo-tree = {
-        enable = true;
-      };
+      neo-tree.enable = true;
       barbar.enable = true;
-      lightline = {
-        enable = true;
-      };
       indent-blankline.enable = true;
       fugitive.enable = true;
+      lualine = {
+        enable = true;
+        theme = "gruvbox-material";
+      };
     };
     extraPlugins = with pkgs.vimPlugins; [
       editorconfig-nvim
@@ -34,6 +37,13 @@
     ];
     colorschemes = {
       gruvbox.enable = true;
+    };
+    maps.normal = {
+      "<leader>t" = "<CMD>NeoTreeShowToggle<CR>";
+      "<leader>ft" = "<CMD>Telescope find_files<CR>";
+      "<leader>fg" = "<CMD>Telescope grep_string<CR>";
+      "j" = "gj";
+      "k" = "gk";
     };
   };
 
