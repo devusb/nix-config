54c9d314603d2de4b14816d0d734329173a4eca0
Morgan Helton <mhelton@gmail.com>
2023-03-19 12:52:42 -0500

    disable Network Manager Wait Online

--- hosts/common/nixos.nix
+++ hosts/common/nixos.nix
@@ -50,4 +50,9 @@
   # install some programs globally
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+
 }
