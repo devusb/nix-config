5252f5ba4da6b6c6a6e53e1dacf301f8beb749d4
Morgan Helton <mhelton@gmail.com>
2023-03-25 00:29:50 -0500

    add steamtinkerlaunch to steam fhsenv

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -1,6 +1,15 @@
+{ pkgs, ... }:
+let
+  steam = pkgs.steam.override {
+    extraPkgs = pkgs: with pkgs; [
+      steamtinkerlaunch
+    ];
+  };
+in
 {
   programs.steam = {
     enable = true;
+    package = steam;
   };
   programs.gamemode.enable = true;
 }
