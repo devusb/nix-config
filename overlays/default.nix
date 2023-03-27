d785a210f14f106c2022b8a78c928c2919defa9a
Morgan Helton <mhelton@gmail.com>
2023-03-26 21:24:50 -0500

    update attic and only expose client

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,7 @@ let
     stable = import inputs.nixpkgs-stable { system = prev.system; };
     mpack = inputs.mpack.packages.${prev.system}.mpack;
     colmena = inputs.colmena.packages.${prev.system}.colmena;
-    attic = inputs.attic.packages.${prev.system}.attic;
+    attic = inputs.attic.packages.${prev.system}.attic-client;
     nixgl = inputs.nixgl.packages.${prev.system};
     nix-search = inputs.nix-search-cli.packages.${prev.system}.nix-search;
   };
