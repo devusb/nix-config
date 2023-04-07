8623b3a0205236ecf172afaa5eddb6467ca50963
Morgan Helton <mhelton@gmail.com>
2023-04-07 16:58:55 -0700

    disable completion snippets for now

--- home/mhelton/neovim.nix
+++ home/mhelton/neovim.nix
@@ -49,28 +49,6 @@
         enable = true;
         autoEnableSources = true;
         sources = [{ name = "nvim_lsp"; }];
-        snippet.expand = "luasnip";
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<Tab>" = {
-            modes = [ "i" "s" ];
-            action = ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expandable() then
-                  luasnip.expand()
-                elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif check_backspace() then
-                  fallback()
-                else
-                  fallback()
-                end
-              end
-            '';
-          };
-        };
       };
     };
     extraPlugins = with pkgs.vimPlugins; [
