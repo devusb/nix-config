3d14df1800fda7b0cf056105d39b45d62132e6e9
Morgan Helton <mhelton@gmail.com>
2023-04-22 07:48:08 -0500

    remove gutenprint overlay, add mutter overlays for window decoration fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,8 +11,21 @@ let
     nix-search = inputs.nix-search-cli.packages.${prev.system}.nix-search;
     devenv = inputs.devenv.packages.${prev.system}.devenv;
 
-    gutenprint = prev.gutenprint.overrideAttrs (old: {
-      nativeBuildInputs = with prev; [ makeWrapper pkg-config cups perl ];
+    gnome = prev.gnome.overrideScope' (gself: gsuper: {
+      mutter = gsuper.mutter.overrideAttrs (old: {
+        patches = [
+          # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2948
+          (prev.fetchurl {
+            url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/20e2adc4fc0549a394a18cf207ad32b42b142d4f.patch";
+            sha256 = "sha256-6uvh/itSYNPlWswYo7a1ESfsIIc/Ag6BTWypouIWlD0=";
+          })
+          # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2954
+          (prev.fetchurl {
+            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2954/diffs.patch";
+            sha256 = "sha256-jFoOd/hctFhkU0tNSNxuMzZTlihYOflkhRv9Szvd0Us=";
+          })
+        ];
+      });
     });
   };
 in
