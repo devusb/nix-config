119d1902112cf15f50ebf59c258baa3218d4b582
Morgan Helton <mhelton@gmail.com>
2023-05-02 23:31:17 -0500

    update flake.lock, remove mutter patches

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,29 +11,6 @@ let
     nix-search = inputs.nix-search-cli.packages.${prev.system}.nix-search;
     devenv = inputs.devenv.packages.${prev.system}.devenv;
 
-    # mutter patches
-    gnome = prev.gnome.overrideScope' (gself: gsuper: {
-      mutter = gsuper.mutter.overrideAttrs (old: {
-        patches = [
-          # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2948
-          (prev.fetchurl {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/20e2adc4fc0549a394a18cf207ad32b42b142d4f.patch";
-            sha256 = "sha256-6uvh/itSYNPlWswYo7a1ESfsIIc/Ag6BTWypouIWlD0=";
-          })
-          # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2954
-          (prev.fetchurl {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2954/diffs.patch";
-            sha256 = "sha256-jFoOd/hctFhkU0tNSNxuMzZTlihYOflkhRv9Szvd0Us=";
-          })
-          # https://gitlab.gnome.org/GNOME/mutter/-/issues/2690
-          (prev.fetchurl {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2952/diffs.patch";
-            sha256 = "sha256-eceXbMeGQCiC9V7XMEN3oDcw4ZmOBh5KAoe1xau6Lg8=";
-          })
-        ];
-      });
-    });
-
     # bump openjk version
     openjk = prev.openjk.overrideAttrs (old: {
       version = "unstable-2023-04-20";
