bc9fbf94c710136dc4a4a38b93ce2dbb2a21885c
Morgan Helton <mhelton@gmail.com>
2023-06-04 14:47:08 -0500

    add mangohud fps lock toggle

--- home/mhelton/gaming.nix
+++ home/mhelton/gaming.nix
@@ -19,9 +19,10 @@
     settings = {
       gl_vsync = -1;
       vsync = 0;
-      fps_limit = 144;
+      fps_limit = "144,60";
       full = true;
       toggle_hud_position = "R_Shift+F10";
+      toggle_fps_limit = "L_Shift+F1";
       position = "top-right";
       no_display = true;
     };
