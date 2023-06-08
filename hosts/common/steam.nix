1bb8ff6e8b3e6050596484bf0aa9fab9ac782962
Morgan Helton <mhelton@gmail.com>
2023-06-08 05:19:54 -0500

    update flake.lock; add steam attr workaround

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -2,6 +2,7 @@
 {
   programs.steam = {
     enable = true;
+    package = with pkgs; steam.override { extraPkgs = pkgs: [ attr ]; };
   };
   programs.gamemode.enable = true;
   environment.systemPackages = with pkgs; [ steamtinkerlaunch xdotool xorg.xwininfo ];
