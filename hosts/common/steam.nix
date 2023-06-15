f37db02d474f8e8eda62faf602d6cf8f1ae7f089
Morgan Helton <mhelton@gmail.com>
2023-06-15 07:36:16 -0500

    update flake.lock

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -2,7 +2,6 @@
 {
   programs.steam = {
     enable = true;
-    package = with pkgs; steam.override { extraPkgs = pkgs: [ attr ]; };
   };
   programs.gamemode.enable = true;
   environment.systemPackages = with pkgs; [ steamtinkerlaunch xdotool xorg.xwininfo ];
