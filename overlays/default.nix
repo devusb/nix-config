cf952c78f6d0591743106a5c8f5bb42053f302e9
Morgan Helton <morgan@imubit.com>
2023-08-01 09:03:38 -0500

    add starship patch for aws-sso-cli

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,6 +38,19 @@ let
       nativeBuildInputs = old.nativeBuildInputs ++ [ prev.pkgs.unixtools.xxd ];
     });
 
+    # starship PR for aws-sso-cli
+    starship = prev.starship.overrideAttrs (old: {
+      patches = [
+        (prev.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/starship/starship/pull/5028.diff";
+          hash = "sha256-k6b57kPs6H6DcloRAB85a8g/ixO5QgtxNKEzzON5PMI=";
+        })
+      ];
+      checkFlags = [
+        "--skip=modules::aws::tests::expiration_date_set"
+      ];
+    });
+
   };
 
 in
