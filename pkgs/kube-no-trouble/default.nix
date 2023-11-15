9ea493aeac4669f5c9379c9d069083e74d6e778c
Morgan Helton <morgan@imubit.com>
2023-11-14 20:13:12 -0600

    bump kubectl-gadget and kube-no-trouble

--- pkgs/kube-no-trouble/default.nix
+++ pkgs/kube-no-trouble/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "kube-no-trouble";
-  version = "nightly-0.7.0-53-g4fa4920";
+  version = "nightly-0.7.0-58-g270c9cb";
 
   src = fetchFromGitHub {
     owner = "doitintl";
     repo = "kube-no-trouble";
     rev = "${version}";
-    hash = "sha256-nMlyRmfsKFkQ18L/RxmVOqoNIWdDyHY7+vZaCOvj4Is=";
+    hash = "sha256-wqlnZg/qzlGlOvwx/0kuehPi4X5mTeDWDfmQ/ecKQbw=";
   };
 
   ldflags = [
@@ -20,7 +20,7 @@ buildGoModule rec {
     "-X main.version=${version}"
   ];
 
-  vendorHash = "sha256-Y0n8tDQeiBdEnCbIrI2je8UCvK72REDBnAH+AtwQf1Q=";
+  vendorHash = "sha256-nEc0fngop+0ju8hDu7nowBsioqCye15Jo1mRlM0TtlQ=";
 
   meta = with lib; {
     homepage = "https://github.com/doitintl/kube-no-trouble";
