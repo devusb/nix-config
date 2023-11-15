1b2534c7e7b3563c15ba2943835d888fb310b88d
Morgan Helton <morgan@imubit.com>
2023-11-15 09:00:28 -0600

    update kor

--- pkgs/kor/default.nix
+++ pkgs/kor/default.nix
@@ -5,18 +5,22 @@
 
 buildGoModule rec {
   pname = "kor";
-  version = "0.2.1";
+  version = "0.2.9";
 
   src = fetchFromGitHub {
     owner = "yonahd";
     repo = "kor";
     rev = "v${version}";
-    hash = "sha256-+MP1kxyB53VXlNtQWMDW9CJmWWzqWKkm5I+6aPwBdWE=";
+    hash = "sha256-+/dsDMjENLqBzEJK6zlc1qcHZPKMVIFEISkw+DINtHY=";
   };
 
-  vendorHash = "sha256-wmRu3uA3AmewJxzKhDNcCHEVHeAKR7jDGGdWgMN//n0=";
+  vendorHash = "sha256-iAqptugku3qX6e45+YYf1bU9j2YntNQj82vR04bFMOQ=";
 
   ldflags = [ "-s" "-w" ];
+
+  checkFlags = [
+    "--skip=TestWriteOutputToFile"
+  ];
 
   meta = with lib; {
     description = "A Tool to discover unused Kubernetes Resouorces";
