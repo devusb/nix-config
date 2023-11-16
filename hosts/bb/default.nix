7586e5c4ebfb91deb4e387a826cbda0621b3dce8
Morgan Helton <mhelton@gmail.com>
2023-11-16 15:26:21 -0600

    change ps5 IP

--- hosts/bb/default.nix
+++ hosts/bb/default.nix
@@ -83,7 +83,7 @@
 
   programs.chiaki4deck = {
     enable = true;
-    consoleAddress = "192.168.10.58";
+    consoleAddress = "192.168.11.7";
     registrationKeyPath = config.sops.secrets.registration_key.path;
     consoleNickname = "PS5-875";
   };
