0be5e89f3e63f8deaa9cecbdaf42664182f8ba51
Morgan Helton <morgan@imubit.com>
2023-12-06 15:36:31 -0600

    skip docker test for vkv

--- pkgs/vkv/default.nix
+++ pkgs/vkv/default.nix
@@ -22,6 +22,11 @@ buildGoModule rec {
     "-X=main.version=${version}"
   ];
 
+  checkFlags = [
+    # requires docker
+    "-skip=TestVaultSuite"
+  ];
+
   meta = with lib; {
     description = "vkv enables you to list, compare, import, document, backup & encrypt secrets from a HashiCorp Vault KV-v2 engine";
     homepage = "https://github.com/FalcoSuessgott/vkv";
