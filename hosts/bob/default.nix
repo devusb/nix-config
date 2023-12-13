bc005447630adc9a93ff1d30c2cd54804108db2c
Morgan Helton <mhelton@gmail.com>
2023-12-12 20:12:04 -0600

    formatting

--- hosts/bob/default.nix
+++ hosts/bob/default.nix
@@ -23,22 +23,24 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.kernelModules = ["uinput" "evdev" "hid_steam"];
-  boot.initrd.preLVMCommands = let
-    deckbd = "${pkgs.deckbd}/bin/deckbd";
-  in ''
-    try=10
-    while true;do
-      ${deckbd} query && break
-      if test $try -le 0;then break; fi
-      sleep 1
-      echo "Waiting for controller to appear, $try retry remains..."
-      try=$((try - 1))
-    done
-    echo "Initialise deckbd";
-    ${deckbd} &
-    DECKBD_PID=$!
-  '';
+  boot.initrd.kernelModules = [ "uinput" "evdev" "hid_steam" ];
+  boot.initrd.preLVMCommands =
+    let
+      deckbd = "${pkgs.deckbd}/bin/deckbd";
+    in
+    ''
+      try=10
+      while true;do
+        ${deckbd} query && break
+        if test $try -le 0;then break; fi
+        sleep 1
+        echo "Waiting for controller to appear, $try retry remains..."
+        try=$((try - 1))
+      done
+      echo "Initialise deckbd";
+      ${deckbd} &
+      DECKBD_PID=$!
+    '';
   boot.initrd.postMountCommands = ''
     kill $DECKBD_PID
   '';
--- hosts/bob/hardware-configuration.nix
+++ hosts/bob/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
