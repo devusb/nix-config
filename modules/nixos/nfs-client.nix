433b39c03372c46b89ef99dadc4ca12d65b97044
Morgan Helton <mhelton@gmail.com>
2024-01-06 21:27:07 -0600

    update nfs-client IPs and paths

--- modules/nixos/nfs-client.nix
+++ modules/nixos/nfs-client.nix
@@ -16,17 +16,17 @@ in
   config = mkIf config.services.nfs-client.enable {
     fileSystems = {
       "/mnt/r2d2/media" = {
-        device = "192.168.20.131:/mnt/media";
+        device = "192.168.20.109:/r2d2_0/media";
         fsType = "nfs";
         options = [ "x-systemd.automount" "noauto" ];
       };
       "/mnt/r2d2/backup" = {
-        device = "192.168.20.131:/mnt/backup";
+        device = "192.168.20.109:/r2d2_0/backup";
         fsType = "nfs";
         options = [ "x-systemd.automount" "noauto" ];
       };
       "/mnt/r2d2/homes" = {
-        device = "192.168.20.131:/mnt/homes";
+        device = "192.168.20.109:/r2d2_0/homes";
         fsType = "nfs";
         options = [ "x-systemd.automount" "noauto" ];
       };
