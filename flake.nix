fc6e2cf8ba6cc049c73ac4378565293294be82d3
Morgan Helton <mhelton@gmail.com>
2024-02-29 05:19:38 -0600

    use nixpkgs plasma6

--- flake.nix
+++ flake.nix
@@ -88,10 +88,6 @@
     # deckbd
     deckbd.url = "github:Ninlives/deckbd";
     deckbd.inputs.nixpkgs.follows = "nixpkgs";
-
-    # kde2nix
-    kde2nix.url = "github:nix-community/kde2nix";
-    kde2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, darwin, flake-parts, hercules-ci-effects, attic, ... }@inputs:
