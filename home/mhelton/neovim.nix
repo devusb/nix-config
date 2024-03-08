23a3bb10215107cff38cff05bc850c8a160cf87e
Morgan Helton <mhelton@gmail.com>
2024-03-08 07:03:07 -0600

    nixvim: translate cmp config to new pattern

--- home/mhelton/neovim.nix
+++ home/mhelton/neovim.nix
@@ -50,14 +50,19 @@
         };
       };
       luasnip.enable = true;
-      nvim-cmp = {
+      cmp = {
         enable = true;
         autoEnableSources = true;
-        sources = [{ name = "nvim_lsp"; }];
-        snippet.expand = "luasnip";
-        mappingPresets = [ "insert" ];
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        settings = {
+          snippet.expand = "luasnip";
+          sources = [{ name = "nvim_lsp"; }];
+          mapping = {
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+          };
         };
       };
       nvim-osc52 = {
