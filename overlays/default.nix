9bc16e453e9310088407e67d46fd149ef226eefc
Morgan Helton <mhelton@gmail.com>
2024-03-13 22:00:10 -0500

    remove unused overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,60 +2,12 @@
 let
   customPkgs = final: prev: import ../pkgs { inherit final prev; };
 
-  modifications = final: prev: rec {
+  modifications = final: prev: {
     stable = import inputs.nixpkgs-stable { system = prev.system; };
     mpack = inputs.mpack.packages.${prev.system}.mpack;
     colmena = inputs.colmena.packages.${prev.system}.colmena;
     attic = inputs.attic.packages.${prev.system}.attic-client;
     devenv = inputs.devenv.packages.${prev.system}.devenv;
-
-    # bump openjk version
-    openjk = prev.openjk.overrideAttrs (old: {
-      version = "unstable-2023-04-20";
-      src = prev.fetchFromGitHub {
-        owner = "JACoders";
-        repo = "OpenJK";
-        rev = "f1448bc2c04d13f259a88dd2a22d6b3c6fa68772";
-        sha256 = "sha256-jTZz1TjjobZmJcwtR3o0QtTWASMSXcUav6oRAEPDMJU=";
-      };
-    });
-
-    # bump john version
-    john = prev.john.overrideAttrs (old: {
-      version = "unstable-2023-06-02";
-      src = prev.fetchFromGitHub {
-        owner = "openwall";
-        repo = "john";
-        rev = "7c73ca9003b2392727fbc4de137fc96a38f82c7e";
-        sha256 = "sha256-TRAxfLBgMA4xQs77ThdRd9uscLNXqF9ycvE6LeYDsSY=";
-      };
-      patches = [ ]; # patches have been upstreamed
-    });
-
-    # bump chiaki for HDR
-    libplacebo_6 = prev.libplacebo.overrideAttrs (old: rec {
-      version = "6.338.1";
-      src = prev.fetchFromGitLab {
-        domain = "code.videolan.org";
-        owner = "videolan";
-        repo = old.pname;
-        rev = "v${version}";
-        hash = "sha256-NZmwR3+lIC2PF+k+kqCjoMYkMM/PKOJmDwAq7t6YONY=";
-      };
-      buildInputs = old.buildInputs ++ [ prev.xxHash ];
-    });
-    chiaki4deck = prev.chiaki4deck.overrideAttrs (old: {
-      buildInputs = old.buildInputs ++ [
-        libplacebo_6
-        prev.vulkan-headers
-        prev.libunwind
-        prev.shaderc
-        prev.vulkan-loader
-        prev.lcms2
-        prev.libdovi
-        prev.xxHash
-      ];
-    });
 
     # joycond
     joycond = prev.joycond.overrideAttrs (old: {
