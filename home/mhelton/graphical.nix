4d86cd7e7287ff539af9e02dc1e860e1f6de8928
Morgan Helton <mhelton@gmail.com>
2024-03-28 17:53:27 -0500

    graphical: add plex-mpv-shim mpv config

--- home/mhelton/graphical.nix
+++ home/mhelton/graphical.nix
@@ -19,26 +19,32 @@
     enable = true;
   };
 
-  xdg.configFile."autostart/1password.desktop".text = ''
-    [Desktop Entry]
-    Categories=Office;
-    Comment[en_US]=Password manager and secure wallet
-    Comment=Password manager and secure wallet
-    Exec=1password %U --silent
-    GenericName[en_US]=
-    GenericName=
-    Icon=1password
-    MimeType=
-    Name[en_US]=1Password
-    Name=1Password
-    Path=
-    StartupNotify=true
-    StartupWMClass=1Password
-    Terminal=false
-    TerminalOptions=
-    Type=Application
-    X-KDE-SubstituteUID=false
-    X-KDE-Username=
-  '';
+  xdg.configFile = {
+    "autostart/1password.desktop".text = ''
+      [Desktop Entry]
+      Categories=Office;
+      Comment[en_US]=Password manager and secure wallet
+      Comment=Password manager and secure wallet
+      Exec=1password %U --silent
+      GenericName[en_US]=
+      GenericName=
+      Icon=1password
+      MimeType=
+      Name[en_US]=1Password
+      Name=1Password
+      Path=
+      StartupNotify=true
+      StartupWMClass=1Password
+      Terminal=false
+      TerminalOptions=
+      Type=Application
+      X-KDE-SubstituteUID=false
+      X-KDE-Username=
+    '';
+    "plex-mpv-shim/mpv.conf".text = ''
+      fs=yes
+      hwdec=vaapi
+    '';
+  };
 
 }
