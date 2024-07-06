83cf33277a80fc650ea9d3db0b15f679122888ab
Morgan Helton <mhelton@gmail.com>
2024-07-06 13:19:12 -0500

    framework: kwin script for firefox window size

--- home/mhelton/framework.nix
+++ home/mhelton/framework.nix
@@ -1,25 +1,32 @@
 { ... }: {
-  programs.plasma.window-rules = [
-    {
-      description = "Firefox HiDPI Initial Size";
-      match = {
-        window-class = {
-          value = "firefox";
-          type = "exact";
-          match-whole = false;
-        };
-        title = {
-          value = "Mozilla Firefox";
-          type = "exact";
-        };
-        window-types = [ "normal" ];
+  xdg.dataFile = {
+    "kwin/scripts/firefoxsize/contents/code/main.js".text = ''
+      var firefoxWidth = 1682;
+      var firefoxHeight = 1084;
+
+      function setWindowSize(client) {
+          if (client.resourceName === "firefox" && client.caption === "Mozilla Firefox" && workspace.activeScreen.devicePixelRatio === 1.25) {
+              client.frameGeometry = {
+                  x: (workspace.activeScreen.geometry.width - firefoxWidth) / 2,
+                  y: ((workspace.activeScreen.geometry.height - firefoxHeight) / 2) - 35,
+                  width: firefoxWidth,
+                  height: firefoxHeight
+              };
+          }
+      }
+
+      workspace.windowAdded.connect(setWindowSize);
+    '';
+    "kwin/scripts/firefoxsize/metadata.json".text = builtins.toJSON {
+      KPlugin = {
+        Name = "Firefox HiDPI Initial Size";
+        Icon = "preferences-system-windows";
+        Id = "firefoxsize";
       };
-      apply = {
-        size = {
-          value = "1682,1084";
-        };
-      };
-    }
-  ];
+      "X-Plasma-API" = "javascript";
+      "X-Plasma-MainScript" = "code/main.js";
+      "KPackageStructure" = "KWin/Script";
+    };
+  };
 
 }
