a6d9f6212ff132fdea2ce742372c769321549738
Morgan Helton <mhelton@gmail.com>
2024-07-21 15:03:42 -0500

    docker: set pids_limit to 0

--- hosts/common/docker.nix
+++ hosts/common/docker.nix
@@ -4,5 +4,8 @@
     dockerCompat = true;
     dockerSocket.enable = true;
   };
+  virtualisation.containers.containersConf.settings = {
+    containers.pids_limit = 0;
+  };
   virtualisation.oci-containers.backend = "podman";
 }
