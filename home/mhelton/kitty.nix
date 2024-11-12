d5b2146d8546e5a98e4031137d2107855c5b5680
Morgan Helton <mhelton@gmail.com>
2024-11-12 05:32:02 -0600

    kitty: set scrollback_pager_history_size

--- home/mhelton/kitty.nix
+++ home/mhelton/kitty.nix
@@ -9,6 +9,7 @@
       background_opacity = "0.9";
       confirm_os_window_close = "2";
       macos_option_as_alt = "yes";
+      scrollback_pager_history_size = "100";
     };
     keybindings = {
       "ctrl+f2" = "detach_tab";
