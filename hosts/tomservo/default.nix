be5b559f314cd18320b659d02d99da1d9ab3a51d
Morgan Helton <mhelton@gmail.com>
2024-12-11 21:01:12 -0600

    tomservo: add mesa-git patch for indy eyes

--- hosts/tomservo/default.nix
+++ hosts/tomservo/default.nix
@@ -72,6 +72,20 @@
       fallbackSpecialisation = false;
     };
   };
+  nixpkgs.overlays = [
+    (final: prev: {
+      mesa_git = prev.mesa_git.overrideAttrs (old: {
+        patches = old.patches ++ [
+          ./indy-eyes.patch
+        ];
+      });
+      mesa32_git = prev.mesa32_git.overrideAttrs (old: {
+        patches = old.patches ++ [
+          ./indy-eyes.patch
+        ];
+      });
+    })
+  ];
 
   # Plasma
   services.displayManager.sddm = {
