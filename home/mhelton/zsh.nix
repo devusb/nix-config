22592115f1e4b0a5dced44800654089cc8fd190d
Morgan Helton <mhelton@gmail.com>
2024-12-27 13:19:23 -0500

    zsh: use getExe

--- home/mhelton/zsh.nix
+++ home/mhelton/zsh.nix
@@ -11,18 +11,18 @@
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = {
-      ga-intent = "${pkgs.git}/bin/git add --intent-to-add";
-      grm-cache = "${pkgs.git}/bin/git rm --cached";
+      ga-intent = "${lib.getExe pkgs.git} add --intent-to-add";
+      grm-cache = "${lib.getExe pkgs.git} rm --cached";
       update =
         if pkgs.stdenv.isDarwin then
           "darwin-rebuild switch --flake $DOTFILES"
         else
           "nixos-rebuild switch --use-remote-sudo --flake $DOTFILES";
       update-boot = lib.mkIf pkgs.stdenv.isLinux "nixos-rebuild boot --use-remote-sudo --flake $DOTFILES";
-      kb = "${pkgs.kubectl}/bin/kubectl";
-      cat = "${pkgs.bat}/bin/bat --paging=always";
-      ts = "${pkgs.tailscale}/bin/tailscale";
-      gl = "${pkgs.git}/bin/git log -p --ext-diff";
+      kb = "${lib.getExe pkgs.kubectl}";
+      cat = "${lib.getExe pkgs.bat} --paging=always";
+      ts = "${lib.getExe pkgs.tailscale}";
+      gl = "${lib.getExe pkgs.git} log -p --ext-diff";
       watch = "${lib.getExe pkgs.watch} --color ";
     };
     history = {
