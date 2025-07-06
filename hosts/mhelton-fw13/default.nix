36acc16954d9d721c995446884fb6b70d4b12566
Morgan Helton <mhelton@gmail.com>
2025-07-05 21:04:42 -0500

    hosts/mhelton-fw13: init

---
+++ hosts/mhelton-fw13/default.nix
@@ -0,0 +1,99 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.framework-amd-ai-300-series
+    inputs.disko.nixosModules.disko
+    # inputs.lanzaboote.nixosModules.lanzaboote
+    (import ./disko-config.nix { disks = [ "/dev/nvme0n1" ]; })
+    inputs.sops-nix.nixosModules.sops
+    ../common/users/mhelton
+    ../common/nixos.nix
+    ../common/steam.nix
+    ../common/_1password.nix
+    ../common/docker.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.lanzaboote = {
+  #   enable = true;
+  #   pkiBundle = "/etc/secureboot";
+  # };
+  boot.loader.systemd-boot.enable = true;
+
+  # boot.blacklistedKernelModules = [ "hid_sensor_hub" ];
+  # boot.extraModprobeConfig = ''
+  #   options snd_hda_intel power_save=1
+  # '';
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  networking.hostName = "mhelton-fw13"; # Define your hostname.
+  networking.networkmanager = {
+    enable = true;
+    wifi.powersave = false;
+    wifi.backend = "iwd";
+  };
+
+  hardware.bluetooth.enable = true;
+  # hardware.sensor.iio.enable = false;
+  hardware.enableAllFirmware = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "modesetting" ];
+  };
+
+  hardware.graphics.enable = true;
+
+  # Plasma
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  security.pam.services.login.fprintAuth = false;
+
+  services.displayManager.defaultSession = "plasma";
+  services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    sbctl
+  ];
+  programs.kdeconnect.enable = true;
+
+  environment.variables = {
+    VDPAU_DRIVER = "radeonsi";
+  };
+
+  services.fwupd.enable = true;
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ gutenprint ];
+  };
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
