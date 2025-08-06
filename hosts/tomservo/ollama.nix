dffde21917da6231af16133109c34e80699ce856
Morgan Helton <mhelton@gmail.com>
2025-08-05 22:00:01 -0500

    chore(hosts/tomservo): bump ollama to 0.11.2

--- hosts/tomservo/ollama.nix
+++ hosts/tomservo/ollama.nix
@@ -6,6 +6,13 @@
     environmentVariables = {
       HSA_OVERRIDE_GFX_VERSION = "11.0.0";
     };
+    package = pkgs.ollama-rocm.overrideAttrs (old: rec {
+      version = "0.11.2";
+      src = old.src.override {
+        tag = "v${version}";
+        hash = "sha256-NZaaCR6nD6YypelnlocPn/43tpUz0FMziAlPvsdCb44=";
+      };
+    });
     host = "0.0.0.0";
   };
 
