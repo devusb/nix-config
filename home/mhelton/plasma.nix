c58621a65e0505e2fb786d23a989c655808d7a7e
Morgan Helton <mhelton@gmail.com>
2025-09-19 08:49:35 -0500

    chore(home/plasma): homeManagerModules -> homeModules

--- home/mhelton/plasma.nix
+++ home/mhelton/plasma.nix
@@ -1,7 +1,7 @@
 { inputs, pkgs, ... }:
 {
   imports = [
-    inputs.plasma-manager.homeManagerModules.plasma-manager
+    inputs.plasma-manager.homeModules.plasma-manager
   ];
 
   home.packages = with pkgs; [
