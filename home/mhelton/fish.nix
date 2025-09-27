ec908bc532cba3c3eaabeb2d6d3a6f1d6cbdf6cc
Morgan Helton <mhelton@gmail.com>
2025-09-26 22:49:01 -0500

    feat(home/fish): enable vi key bindings

--- home/mhelton/fish.nix
+++ home/mhelton/fish.nix
@@ -22,6 +22,7 @@
     };
     interactiveShellInit = ''
       set fish_greeting
+      fish_vi_key_bindings
     '';
     functions = {
       nr = import ./extra/fish/nr.nix { inherit pkgs lib; };
