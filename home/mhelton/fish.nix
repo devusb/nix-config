e36fd891729cecac650c6c601c20ac1054d19591
Morgan Helton <mhelton@gmail.com>
2025-09-26 22:54:21 -0500

    chore(home): centralize shell aliases

--- home/mhelton/fish.nix
+++ home/mhelton/fish.nix
@@ -2,24 +2,7 @@
 {
   programs.fish = {
     enable = true;
-    shellAliases = {
-      ga-intent = "${lib.getExe pkgs.git} add --intent-to-add";
-      grm-cache = "${lib.getExe pkgs.git} rm --cached";
-      update =
-        if pkgs.stdenv.isDarwin then
-          "darwin-rebuild switch --flake $DOTFILES"
-        else
-          "nixos-rebuild switch --sudo --flake $DOTFILES";
-      update-boot = lib.mkIf pkgs.stdenv.isLinux "nixos-rebuild boot --sudo --flake $DOTFILES";
-      update-test = lib.mkIf pkgs.stdenv.isLinux "nixos-rebuild test --sudo --flake $DOTFILES";
-      kb = "${lib.getExe pkgs.kubectl}";
-      cat = "${lib.getExe pkgs.bat} --paging=always";
-      ts = "${lib.getExe pkgs.tailscale}";
-      gl = "${lib.getExe pkgs.git} log -p --ext-diff";
-      watch = "${lib.getExe pkgs.watch} --color ";
-      t = "${lib.getExe pkgs.tmux}";
-      ta = "${lib.getExe pkgs.tmux} attach";
-    };
+    shellAliases = import ./extra/aliases.nix { inherit pkgs lib; };
     interactiveShellInit = ''
       set fish_greeting
       fish_vi_key_bindings
--- home/mhelton/zsh.nix
+++ home/mhelton/zsh.nix
@@ -10,24 +10,7 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
-    shellAliases = {
-      ga-intent = "${lib.getExe pkgs.git} add --intent-to-add";
-      grm-cache = "${lib.getExe pkgs.git} rm --cached";
-      update =
-        if pkgs.stdenv.isDarwin then
-          "darwin-rebuild switch --flake $DOTFILES"
-        else
-          "nixos-rebuild switch --sudo --flake $DOTFILES";
-      update-boot = lib.mkIf pkgs.stdenv.isLinux "nixos-rebuild boot --sudo --flake $DOTFILES";
-      update-test = lib.mkIf pkgs.stdenv.isLinux "nixos-rebuild test --sudo --flake $DOTFILES";
-      kb = "${lib.getExe pkgs.kubectl}";
-      cat = "${lib.getExe pkgs.bat} --paging=always";
-      ts = "${lib.getExe pkgs.tailscale}";
-      gl = "${lib.getExe pkgs.git} log -p --ext-diff";
-      watch = "${lib.getExe pkgs.watch} --color ";
-      t = "${lib.getExe pkgs.tmux}";
-      ta = "${lib.getExe pkgs.tmux} attach";
-    };
+    shellAliases = import ./extra/aliases.nix { inherit pkgs lib; };
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
