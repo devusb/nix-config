76074e4d8a2198923a1e47816b71abfcb4095a48
Morgan Helton <morgan@flox.dev>
2025-11-17 14:08:25 -0600

    feat(home/ghostty): add configuration

--- home/mhelton/ghostty.nix
+++ home/mhelton/ghostty.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ config, ... }:
 {
   programs.ghostty = {
     enable = true;
+    enableFishIntegration = config.programs.fish.enable;
+    enableZshIntegration = config.programs.zsh.enable;
+    settings = {
+      window-height = 45;
+      window-width = 170;
+      shell-integration-features = "ssh-env";
+      background-opacity = 0.97;
+    };
   };
 }
