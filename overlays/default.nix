617d9a7911553c12c9e87f21036ea25dacf689e6
Morgan Helton <mhelton@gmail.com>
2026-01-16 09:34:42 -0600

    chore: bump git-absorb (#491)

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,6 +22,15 @@ let
         '';
       });
 
+      git-absorb = prev.git-absorb.overrideAttrs (old: {
+        version = "0.8.0-unstable-2025-09-13";
+        src = old.src.override {
+          tag = null;
+          rev = "3a1148ea2df3ca41cb69df8848f99d25e66dc0b5";
+          hash = "sha256-CrpLWDHSnT2PgbLFDK6UyaeKgmW1mygvSIudsl/nbbQ=";
+        };
+      });
+
       pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
         (self: super: {
           # skip additional tests that seem to require network access
