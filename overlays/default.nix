a799c0d8552d100b3fa777d0c955efbd246b364f
Morgan Helton <mhelton@gmail.com>
2026-01-25 19:54:00 -0600

    chore: properly override git-absorb

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,12 +22,17 @@ let
         '';
       });
 
-      git-absorb = prev.git-absorb.overrideAttrs (old: {
+      git-absorb = prev.git-absorb.overrideAttrs (old: rec {
         version = "0.8.0-unstable-2025-09-13";
         src = old.src.override {
           tag = null;
           rev = "3a1148ea2df3ca41cb69df8848f99d25e66dc0b5";
           hash = "sha256-CrpLWDHSnT2PgbLFDK6UyaeKgmW1mygvSIudsl/nbbQ=";
+        };
+        cargoDeps = old.cargoDeps.overrideAttrs {
+          inherit src;
+          outputHashMode = "recursive";
+          outputHash = "sha256-03vHVC3PSmHMLouQSirPlIG5o7BpvgWjFCtKLAGnxg8";
         };
       });
 
