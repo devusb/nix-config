06d47b43a106ae257e7d1fbfccc66ab375b9871a
Morgan Helton <morgan@flox.dev>
2026-05-22 17:14:24 -0500

    chore(hosts/common): swap to docker

--- hosts/common/docker.nix
+++ hosts/common/docker.nix
@@ -1,13 +1,8 @@
 { ... }:
 {
-  virtualisation.podman = {
+  virtualisation.docker = {
     enable = true;
-    dockerCompat = true;
-    dockerSocket.enable = true;
   };
-  virtualisation.containers.containersConf.settings = {
-    containers.pids_limit = 0;
-  };
-  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.backend = "docker";
 
 }
--- hosts/common/users/mhelton/default.nix
+++ hosts/common/users/mhelton/default.nix
@@ -11,6 +11,7 @@
       extraGroups = [
         "wheel"
         "podman"
+        "docker"
         "libvirtd"
         "networkmanager"
         "keys"
