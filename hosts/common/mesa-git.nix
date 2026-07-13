e32da5d71e135c3908d4fe8fe3c7151566de6ae9
Morgan Helton <mhelton@gmail.com>
2026-07-13 08:46:12 -0500

    chore(hosts/common/mesa-git): remove doom patch

--- hosts/common/mesa-git.nix
+++ hosts/common/mesa-git.nix
@@ -3,27 +3,18 @@
     inputs.chaotic.nixosModules.default
   ];
 
-  nixpkgs.overlays =
-    let
-      doomPatch = pkgs.fetchpatch {
-        url = "https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42768.patch";
-        hash = "sha256-Qb3Iz2OpHUzrYab0ivOxiAhGbWvC8SUwE9pU/fhBZAk";
-      };
-    in
-    [
-      (final: prev: {
-        mesa_git = prev.mesa_git.overrideAttrs (old: {
-          patches = old.patches ++ [
-            doomPatch
-          ];
-        });
-        mesa32_git = prev.mesa32_git.overrideAttrs (old: {
-          patches = old.patches ++ [
-            doomPatch
-          ];
-        });
-      })
-    ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      mesa_git = prev.mesa_git.overrideAttrs (old: {
+        patches = old.patches ++ [
+        ];
+      });
+      mesa32_git = prev.mesa32_git.overrideAttrs (old: {
+        patches = old.patches ++ [
+        ];
+      });
+    })
+  ];
 
   specialisation.mesa-git.configuration = {
     chaotic.mesa-git = {
