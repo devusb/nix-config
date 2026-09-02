7afc3be152c4e17b333b8fedd99910d89f1b8063
Morgan Helton <mhelton@gmail.com>
2026-09-01 20:32:58 -0500

    refactor(home/niri): split window rules into their own file

---
+++ home/mhelton/niri/window-rules.nix
@@ -0,0 +1,68 @@
+{ ... }:
+{
+  wayland.windowManager.niri.settings = {
+    _children = [
+      {
+        window-rule._children = [
+          {
+            match._props = {
+              app-id = "firefox$";
+              title = "^Picture-in-Picture$";
+            };
+          }
+          { open-floating = true; }
+          { default-column-width.fixed = 480; }
+          { default-window-height.fixed = 270; }
+        ];
+      }
+      {
+        window-rule._children = [
+          { match._props.app-id = "^1Password$"; }
+          { block-out-from = "screen-capture"; }
+        ];
+      }
+      {
+        window-rule._children = [
+          {
+            match._props = {
+              app-id = "^zoom$";
+              title = "^zoom_linux_float_video_window$";
+            };
+          }
+          { open-floating = true; }
+          {
+            default-floating-position._props = {
+              x = 16;
+              y = 16;
+              relative-to = "bottom-right";
+            };
+          }
+        ];
+      }
+      {
+        window-rule._children = [
+          {
+            match._props = {
+              app-id = "^zoom$";
+              title = "^as_toolbar$";
+            };
+          }
+          {
+            match._props = {
+              app-id = "^zoom$";
+              title = "^as_preview$";
+            };
+          }
+          { open-floating = true; }
+          {
+            default-floating-position._props = {
+              x = 16;
+              y = 16;
+              relative-to = "top-right";
+            };
+          }
+        ];
+      }
+    ];
+  };
+}
