0bde8bfc35f801ff84a53f21899710008cf64b1b
Morgan Helton <mhelton@gmail.com>
2026-09-06 21:14:24 -0500

    chore(hosts/r2d2): disable bluetooth on boot

--- hosts/r2d2/default.nix
+++ hosts/r2d2/default.nix
@@ -49,7 +49,10 @@
     wifi.backend = "iwd";
   };
 
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false;
+  };
   hardware.sensor.iio.enable = false;
   hardware.enableAllFirmware = true;
 
